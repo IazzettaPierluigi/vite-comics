6027b5b2ebbfbf7c96bf927136e87cbc8a3135b2
IazzettaPierluigi <iazzettapierluigi@gmail.com>
2024-02-07 16:20:57 +0100

    creating header elements

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -6,11 +6,57 @@ export default {
 </script>
 
 <template>
-    <h1>sono proprio io, l'header</h1>
+    <header>
+
+        <figure>
+            <img src="../assets/img/dc-logo.png" alt="">
+        </figure>
+        <div class="lista">
+            <ul>
+                <li>characters</li>
+                <li>comics</li>
+                <li>movies</li>
+                <li>tv</li>
+                <li>games</li>
+                <li>collectibles</li>
+                <li>videos</li>
+                <li>fans</li>
+                <li>news</li>
+                <li>shop</li>
+            </ul>
+        </div>
+    </header>
 </template>
 
 <style lang="scss">
-h1 {
-    color: red;
+header {
+    //style di header
+    width: 100%;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+
+    figure {
+        width: 100px;
+        background-color: red;
+
+        img {
+            width: 100%;
+        }
+    }
+
+    .lista {
+        //style del div con classe lista
+
+        ul {
+            //style della UL
+            display: flex;
+            flex-direction: row;
+
+            li {
+                list-style-type: none;
+            }
+        }
+    }
 }
 </style>
