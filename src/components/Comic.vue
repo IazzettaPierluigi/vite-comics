22ad432d19aa2c25ce61687a61ee869a2770a568
IazzettaPierluigi <iazzettapierluigi@gmail.com>
2024-02-08 17:26:35 +0100

    props with imgs

--- src/components/Comic.vue
+++ src/components/Comic.vue
@@ -2,11 +2,39 @@
 //facciamo l'export
 export default {
     name: "Comic",
+
+    props: {
+        propsImg: String,
+    }
 };
 
 
 </script>
 
-<template></template>
+<template>
+    <div class="col-3">
+        <figure>
+            <img :src="propsImg" alt="">
+        </figure>
+        <h4></h4>
+    </div>
+</template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.col-3 {
+    width: calc(100% /6);
+    height: 200px;
+
+    figure {
+        width: 100%;
+        height: 100%;
+
+        img {
+            width: 100%;
+            height: 100%;
+        }
+    }
+
+    h4 {}
+}
+</style>
